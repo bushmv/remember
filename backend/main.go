9d9aea1d1bc37e7f9965690e454979989ead263f
Bushmakin Vadim <bushmvadim@gmail.com>
2026-02-20 22:05:28 +0300

    feat: add min working version of server with go gin framework

---
+++ backend/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/test", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
+	})
+
+	if err := r.Run(":8080"); err != nil {
+		log.Fatalf("failed to run server: %v", err)
+	}
+}
